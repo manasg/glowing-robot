7fee098b6909286c439be82936c62c4b05884d72
Manas Gupta <manas.gupta@gmail.com>
2013-05-10 00:30:18 -0400

    Naive single threaded py web server

---
+++ server.py
@@ -0,0 +1,43 @@
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+import datetime
+
+# thank you coding horror!
+import re
+pattern = re.compile('(x+x+)+y')
+def compute_regex(str_len=20):
+    string = "x" * str_len
+    result = pattern.match
+
+    if result:
+        return result.span()
+    else:
+        return "no_match"
+
+import math
+def compute_factorial(x=20):
+    return math.factorial(x)
+
+class ReqHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        start = datetime.datetime.now()
+        
+        num_digits = len(str(compute_factorial(x=80000)))
+        resp = "Number of digits %d " % num_digits
+        
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(resp)
+
+        end = datetime.datetime.now()
+        print "Took %s seconds for req" % str(end - start)
+    
+def serve():
+    server = HTTPServer(('localhost',9090), ReqHandler)
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print "terminated!"
+
+
+if __name__ == "__main__":
+    serve() 
